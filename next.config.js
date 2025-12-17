49610783394661dccf1e20dc5c47cd55653ef2d8
Russell Winters <russellwinters@protonmail.com>
2025-12-16 16:11:28 -0800

    updating next config for env variables

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    DEEPL_API_KEY: process.env.DEEPL_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  }
 }
 
 module.exports = nextConfig
